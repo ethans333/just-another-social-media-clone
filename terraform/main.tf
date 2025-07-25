5aeb24896383e1cd86d0a04a8d2c1cb44c419b70
estein <ethanmstn33@gmail.com>
2025-07-25 10:26:20 -0400

    Put terraform backend in s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket  = "jasmc-terraform-state-bucket"
+    key     = "envs/prod/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
 
 provider "aws" {
